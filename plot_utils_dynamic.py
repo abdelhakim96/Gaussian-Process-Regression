5e91a6e119f3821ecc22a87fd30df117a6c936d0
Hakim <abdelhakim@aucegypt.edu>
2023-05-14 20:12:48 +0200

    fixed plots for data

--- plot_utils_dynamic.py
+++ plot_utils_dynamic.py
@@ -3,20 +3,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_gp_dynamic(y, t, x_s, mu, cov, mu_s, cov_s):
+def plot_gp_dynamic(y_all,y, t_all,t, x_s, mu, cov, mu_s, cov_s,i):
     # Select the last 100 elements
 
 
 
-    plt.rcParams["figure.figsize"] = (10, 4)
+    plt.rcParams["figure.figsize"] = (14, 6)
 
     # Clear the current figure
 
     plt.clf()
     # Plotting the figure with data, mean, and uncertainty
 
-    plt.scatter(t, y , color='red', label='Data')
-    plt.scatter(t[len(t)-1]+1, mu , color='blue', label='prediction')
+    plt.scatter(t_all, y_all , color='red', label='Past Data')
+    plt.plot(t, y , color='black', label='Regression Data')
+
+    plt.scatter(t_all[len(t_all)-1]+1, mu , color='blue', label='prediction')
+
 
 
     plt.xlabel('x')
@@ -32,7 +35,8 @@ def plot_gp_dynamic(y, t, x_s, mu, cov, mu_s, cov_s):
 
 
     # Set the x-axis limits
-    #plt.xlim(x_min, x_max)
+    #plt.xlim(t_all[0] , t_all[len(t_all)-1] + 1)
+    plt.xlim(t_all[0]+i +100 , t_all[len(t_all)-1] + 1)
 
     # Update the plot
     plt.pause(0.01)  # Pause to allow the plot to update
